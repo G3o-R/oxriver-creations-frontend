8c3fcd7b1c58eb1b54a97d0260a1d23e5feac6b0
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2024-10-10 18:48:16 -0700

    added hero-slider npm

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,10 @@ import { HomePage } from "../styles/HomeStyles";
 export default function Home(){
     return(
     <HomePage>
-        
+        {/* hero section */}
+        {/* Products: 3d prints, aprons, etc */}
+        {/* About Us */}
+        test
     </HomePage>
     )
 }
