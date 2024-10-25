f66bcc551d1821303691ef75da3ae71d8e2456ea
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2024-10-25 10:06:44 -0700

    building products page

--- src/styles/ProductDisplayStyles.js
+++ src/styles/ProductDisplayStyles.js
@@ -4,8 +4,7 @@ export const ProductDisplayWrapper = styled.div`
   display: flex;
   justify-content: center;
   width: 100%;
-  /* background-color: #454545; */
-`;
+  `;
 
 export const ProductDisplayContainer = styled.div`
   position: relative;
@@ -52,7 +51,6 @@ export const Overlay = styled.div`
   height: 100%;
   display: flex;
   justify-content: center;
-  align-items: center;
   background: rgba(0, 0, 0, 0.4);
   color: #fff;
   text-align: center;
@@ -66,7 +64,7 @@ export const Overlay = styled.div`
   h1 {
     font-size: 2rem;
     font-weight: bold;
-    margin: 0;
+    margin-top: 8rem;
   }
 `;
 
@@ -76,35 +74,39 @@ export const ProductCarouselWrapper = styled.div`
   align-items: center;
   overflow: hidden;
   width: 100%;
-  max-width: 1560px;
   padding: 0;
   z-index: 2;
   margin-top: -175px;
-  
+
+  border-bottom: 4px solid #ffffff; 
+  border-bottom-left-radius: 35%;
+  border-bottom-right-radius: 35%;
+  /* box-shadow */
+
   @media screen and (min-width: 1080px) {
     margin-top: -150px;
-}
-
-@media screen and (min-width: 1325px) {
+  }
+    
+  @media screen and (min-width: 1325px) {
     margin-top: -125px;
-}
-
-@media screen and (min-width: 1560px) {
-      margin-top: -100px;
+  }
+    
+  @media screen and (min-width: 1560px) {
+    margin-top: -100px;
   }
 `;
 
 export const ProductCarouselContainer = styled.div`
   position: relative;
-  
+  max-width: 1560px;
   overflow: hidden;
   width: 100%;
   height: 560px;
   display: flex;
-
+  
   .swiper {
-    width: 100%;
-    height: 100%;
+      width: 100%;
+      height: 100%;
   }
 
   .swiper-slide {
