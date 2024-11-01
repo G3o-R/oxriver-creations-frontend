8e900964ef7b52627a5063dbd06db143b7b07454
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2024-11-01 13:52:24 -0700

    building ProductOverlayDisplay

--- src/components/ProductOverlayDisplay.js
+++ src/components/ProductOverlayDisplay.js
@@ -1,2 +1,49 @@
+import { 
+    CloseBtn,
+    ContentContainer,
+    ContentWrapper,
+    DescriptionContainer,
+    DescriptionWrapper,
+    DisplayContainer,
+    DisplayWrapper,
+    ImageContainer,
+    ImageWrapper,
+    OverlayContainer,
+    PhotoSlidesContainer,
+    PhotoSlidesWrapper,
+ } from "../styles/ProductOverlayDisplayStyles";
 
 
+export default function ProductOverlayDisplay(){
+
+    return(
+        <OverlayContainer>
+            <CloseBtn>x</CloseBtn>
+            <DisplayWrapper>
+                <DisplayContainer>
+                    <ContentWrapper>
+                        <ContentContainer>
+                            <ImageWrapper>
+                                <ImageContainer>
+                                    <img src="" alt="random text" />
+                                </ImageContainer>
+                            </ImageWrapper>
+                            <DescriptionWrapper>
+                                <DescriptionContainer>
+                                    <h1>product name</h1>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                </DescriptionContainer>
+                            </DescriptionWrapper>
+                        </ContentContainer>
+                    </ContentWrapper>
+                    <PhotoSlidesWrapper>
+                        <PhotoSlidesContainer>
+                            {/* photos arr here */}
+                        </PhotoSlidesContainer>
+                    </PhotoSlidesWrapper>
+                </DisplayContainer>
+            </DisplayWrapper>
+
+        </OverlayContainer>
+    )
+}
---
+++ src/styles/ProductOverlayDisplayStyles.js
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+export const OverlayContainer = styled.div`
+`;
+
+export const CloseBtn = styled.div`
+
+`;
+
+export const DisplayWrapper = styled.div`
+
+`;
+
+export const DisplayContainer = styled.div`
+
+`;
+
+export const ContentWrapper = styled.div`
+
+`;
+
+export const ContentContainer = styled.div`
+
+`;
+
+export const ImageWrapper = styled.div`
+
+`;
+
+export const ImageContainer = styled.div`
+
+img{
+
+}
+`;
+
+export const DescriptionWrapper = styled.div`
+
+`;
+
+export const DescriptionContainer = styled.div`
+
+h1{
+
+}
+
+p{
+
+}
+`;
+
+export const PhotoSlidesWrapper = styled.div`
+
+`;
+
+export const PhotoSlidesContainer = styled.div`
+
+`;
